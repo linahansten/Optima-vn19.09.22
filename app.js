3113fd7c95b37a661c295b2dba4032f290cfc365
linahansten <skolkontolina@gmail.com>
2022-11-21 13:33:09 +0200

    Optima clone

---
+++ app.js
@@ -0,0 +1,6 @@
+const el = document.querySelector("content-dropdown");
+
+document.querySelector("content-dropdown").addEventListener("click", function () {
+
+    if (button == "click") alert("hej");
+})
